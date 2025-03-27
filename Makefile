754016f0e1a9927eab5dcf83684524dfca811404
Alex <alex@Alexs-Laptop.hitronhub.home>
2025-03-26 20:32:50 -0400

    update Makefile and userrepository files

--- Makefile
+++ Makefile
@@ -4,6 +4,6 @@
 
 .PHONY: test
 test:
-	go test -v -race timeout 30s ./...	
+	go test -v -race -timeout=30s ./...	
 
 # .DEFAULT_GOAL := build
--- internal/app/store/userrepository_test.go
+++ internal/app/store/userrepository_test.go
@@ -12,14 +12,7 @@ func TestUserRepository_Create(t *testing.T) {
 	s, teardown := store.TestStore(t, databaseURL)
 	defer teardown("users")
 
-	u, err := s.User().Create(&model.User{
-		Email: "user@exl.com",
-	})
-	assert.NoError(t, err)
-	assert.NotNil(t, u)
-	assert.NoError(t, err)
-	assert.NotNil(t, u)
-
+	u, err := s.User().Create(model.TestUser(t))
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
@@ -30,12 +23,12 @@ func TestUserRepository_FindByEmail(t *testing.T) {
 
 	email := "user@exl.com"
 	_, err := s.User().FindByEmail(email)
-
 	assert.Error(t, err)
 
-	u, err := s.User().Create(&model.User{
-		Email: "user@exl.com",
-	})
+	u := model.TestUser(t)
+	u.Email = email
+
+	s.User().Create(u)
 	u, err = s.User().FindByEmail(email)
 
 	assert.NoError(t, err)
